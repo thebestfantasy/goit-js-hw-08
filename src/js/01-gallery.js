91e9457244e2dc53e4730abb73edad4ebae66fcf
thebestfantasy <sergej.fronch@gmail.com>
2023-04-15 22:11:05 +0300

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`);
+
+container.insertAdjacentHTML('beforeend', markup.join(''));
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+});
