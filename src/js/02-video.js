290a93759afdd7dbc965d76c85ea2c1835527a21
thebestfantasy <sergej.fronch@gmail.com>
2023-04-17 05:24:04 +0300

    23

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,20 @@
-{/* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>
-
-<script src="https://player.vimeo.com/api/player.js"></script>
-<script>
     const iframe = document.querySelector('iframe');
     const player = new Vimeo.Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
+
+    player.on('timeupdate', function (data) {
+        localStorage.setItem('vimeoPlayerTime', data.seconds);
     });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-</script> */}
+    const savedTime = localStorage.getItem('vimeoPlayerTime');
+    if (savedTime) {
+        player.setCurrentTime(savedTime);
+    }
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
+
