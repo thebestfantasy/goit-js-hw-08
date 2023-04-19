ecc244f88fc6018d6a87fd0225cc8de6425579f6
thebestfantasy <sergej.fronch@gmail.com>
2023-04-19 08:50:12 +0000

    Deploying to gh-pages from @ thebestfantasy/goit-js-hw-08@179320f73df6f925a718d4d8785bc40d4dac2c6a 🚀

--- 02-video.2162a91c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeupdate",(function(e){localStorage.setItem("vimeoPlayerTime",e.seconds)}));var o=localStorage.getItem("vimeoPlayerTime");o&&t.setCurrentTime(o)}();
-//# sourceMappingURL=02-video.2162a91c.js.map
--- 02-video.c0b63d3f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeupdate",(function(e){localStorage.setItem("vimeoPlayerTime",e.seconds)}));const o=localStorage.getItem("vimeoPlayerTime");o&&t.setCurrentTime(o);
-//# sourceMappingURL=02-video.c0b63d3f.js.map
